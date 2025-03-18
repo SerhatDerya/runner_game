7f902b85cb655bdd675d8d0114b3c0a3bf619560
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2025-03-18 13:42:17 +0300

    UI added for Game Start/Over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     private bool isGameOver = false;
     public static event Action<int> OnLaneChange; // şerit değişimi için event
+
+    [SerializeField] private GameObject gameOverCanvas;
 
     void Start()
     {
@@ -25,11 +29,17 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void GameStart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
     public void GameOver()
     {
         if (!isGameOver)
         {
             isGameOver = true;
+            gameOverCanvas.SetActive(true);
             Debug.Log("Game Over!");
             Time.timeScale = 0f;
             // Oyunun bitmesiyle ilgili diğer işlemler (örneğin, UI güncelleme, ses çalma, vb.)
@@ -38,7 +48,19 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame()
     {
+        SceneManager.LoadScene("GameScene");
+        gameOverCanvas.SetActive(false);
+        isGameOver = false;
         Time.timeScale = 1f; // Oyunu devam ettir
         // Yeniden başlatma işlemleri (örneğin, sahneyi yeniden yükleme)
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        gameOverCanvas.SetActive(false);
+        isGameOver = false;
+        Time.timeScale = 1f; // Oyunu devam ettir
+        // Ana menüye dönme işlemleri (örneğin, sahneyi yeniden yükleme)
     }
 }
