fcd9d495428468d93870ab7e5ac5b131062f1e22
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2025-04-08 14:43:26 +0300

    music and sfx control menu added

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    
     [Header("---------- Audio Source ----------")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource sfxSource;
 
     [Header("---------- Audio Clip ----------")]
-    public AudioClip bakcground;
+    public AudioClip background;
     public AudioClip pain;
     public AudioClip coin;
     public AudioClip swipe;
@@ -21,16 +26,61 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+            sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+            musicSource.mute = PlayerPrefs.GetInt("MusicMute") == 1;
+            sfxSource.mute = PlayerPrefs.GetInt("SFXMute") == 1;
+        }
         musicSource.volume = musicVolume;
         sfxSource.volume = sfxVolume;
-        musicSource.clip = bakcground;
+        
+        musicSource.clip = background;
         musicSource.Play();
         musicSource.loop = true;
+
+        musicSlider.value = musicVolume;
+        sfxSlider.value = sfxVolume;
+
+        musicSlider.onValueChanged.AddListener(delegate { ChangeMusicVolume(musicSlider.value); });
+        sfxSlider.onValueChanged.AddListener(delegate { ChangeSFXVolume(sfxSlider.value); });
+        
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+        PlayerPrefs.Save();
     }
 
     public void PlaySFX(AudioClip clip)
     {
         sfxSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        musicSource.volume = musicVolume;
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.Save();
+    }
+    public void ChangeSFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        sfxSource.volume = sfxVolume;
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+        PlayerPrefs.Save();
+    }
+    public void MuteMusic()
+    {
+        musicSource.mute = !musicSource.mute;
+        PlayerPrefs.SetInt("MusicMute", musicSource.mute ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+    public void MuteSFX()
+    {
+        sfxSource.mute = !sfxSource.mute;
+        PlayerPrefs.SetInt("SFXMute", sfxSource.mute ? 1 : 0);
+        PlayerPrefs.Save();
     }
 
 }
